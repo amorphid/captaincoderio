25078460e8005efc797730a47d9115e923488a76
Michael Pope <amorphid@captaincoder.io>
2013-08-05 13:31:24 -0700

    Section 5:  Wrote integration tests for adding new articles

---
+++ spec/features/creating_a_post_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe "Creating an article" do
+  context "using the new form" do
+    it "should succeed when form is complete" do
+      pending
+    end
+
+    it "should fail when missing name" do
+      pending
+    end
+
+    it "should fail when missing body" do
+      pending
+    end
+  end
+end
