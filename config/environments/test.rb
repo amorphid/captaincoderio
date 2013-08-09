a58d389b73554ddfe272154b300f44d25f854092
Michael Pope <amorphid@captaincoder.io>
2013-08-09 15:49:28 -0700

    Fix bug that prevents destroy actions in production

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -6,7 +6,7 @@ Blog::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.action_dispatch.show_exceptions = false
-  config.action_controller.allow_forgery_protection = false
+  config.action_controller.allow_forgery_protection = true # default is false
   config.action_mailer.delivery_method = :test
   config.active_support.deprecation = :stderr
 end
