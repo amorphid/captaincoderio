752c15cc9ceacadc88f00781b2893ab23c6c445b
Michael Pope <amorphid@captaincoder.io>
2013-08-11 23:22:08 -0700

    Tweaked produciton environment to make Devise compatible with Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,15 +1,22 @@
 Blog::Application.configure do
-  config.cache_classes = true
-  config.eager_load = true
-  config.consider_all_requests_local       = false
+  # Default settings
   config.action_controller.perform_caching = true
-  config.serve_static_assets = false
-  config.assets.js_compressor = :uglifier
-  config.assets.compile = true
-  config.assets.digest = true
-  config.assets.version = '1.0'
-  config.log_level = :info
-  config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
+  config.assets.digest = true
+  config.assets.js_compressor = :uglifier
+  config.assets.version = '1.0'
+  config.cache_classes = true
+  config.consider_all_requests_local = false
+  config.i18n.fallbacks = true
   config.log_formatter = ::Logger::Formatter.new
+  config.log_level = :info
+  config.eager_load = true
+  config.serve_static_assets = false
+
+  # False by default.  True fixed a bug with destroy actions in articles.
+  # https://github.com/amorphid/blog/tree/a58d389b73554ddfe272154b300f44d25f854092
+  config.assets.compile = true
+
+  # Enables Devise to run properly on Heroku
+  config.assets.initialize_on_precompile = false
 end
